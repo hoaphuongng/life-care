cd1127d4044b7bb2e71efa97bce60630cc94f376
Hoa Phuong <hoaphuong310596@gmail.com>
2019-04-09 23:33:25 +0300

    Precache Application Shell and access to Geolocation

--- public/src/js/claim.js
+++ public/src/js/claim.js
@@ -3,11 +3,17 @@ var closeClaimFormBtn = document.querySelector('#close-claim-form-btn');
 var claimFormArea = document.querySelector('#create-claim');
 var claimForm = document.querySelector('#claim-form');
 
+var addressInput = document.querySelector('#claimer_address');
 
+var locationBtn = document.querySelector('#location-btn');
+var locationSpinner = document.querySelector('#location-spinner');
+var fetchedLocation = { lat: 0, long: 0 };
 
-// Click floating button "+" to open claim form 
+// Click floating button "+" to open claim form, init location
 function openClaimForm() {
-	claimFormArea.style.transform = 'translateY(0)';
+	setTimeout(function() {
+		claimFormArea.style.transform = 'translateY(0)';
+	}, 1);
 
 	if (deferredPrompt) {
 		deferredPrompt.prompt();
@@ -16,14 +22,70 @@ function openClaimForm() {
 		});
 		deferredPrompt = null;
 	}
+
+	initLocation();
 }
 
 openClaimFormBtn.addEventListener('click', openClaimForm);
 
 // Close & reset claim form
 function closeClaimForm() {
-	claimFormArea.style.transform = 'translateY(100vh)';
-	claimForm.reset();
+	setTimeout(function() {
+		claimFormArea.style.transform = 'translateY(100vh)';
+		claimForm.reset();
+	}, 1);
+	
+	
+	locationBtn.style.display = 'inline';
+	locationSpinner.style.display = 'none';
 }
 
 closeClaimFormBtn.addEventListener('click', closeClaimForm);
+
+
+// Get location
+locationBtn.addEventListener('click', function() {
+	locationBtn.style.display = 'none';
+	locationSpinner.style.display = 'block';
+
+	if (!('geolocation' in navigator)) {
+		return;
+	}
+
+	navigator.geolocation.getCurrentPosition(
+		function(position) {
+			locationBtn.style.display = 'inline';
+			locationSpinner.style.display = 'none';
+			fetchedLocation = { lat: position.coords.latitude, long: position.coords.longitude };
+			fetchLocation(fetchedLocation.lat, fetchedLocation.long);
+			document.querySelector('#manual-location').classList.add('is-focused');
+		},
+		function(err) {
+			locationBtn.style.display = 'inline';
+			locationSpinner.style.display = 'none';
+			fetchedLocation = { lat: 0, long: 0 };
+		},
+		{ timeout: 7000 });
+});
+
+// Check if we have Geolocation
+function initLocation() {
+	if (!('geolocation' in navigator)) {
+		locationBtn.style.display = none;
+	}
+}
+
+// Fetch location 
+function fetchLocation(lat, long) {
+	fetch('https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + lat + '&lon=' + long)
+	.then(function(res) {
+		return res.clone();
+	})
+	.then(function(resClone) {
+		return resClone.json();
+	})
+	.then(function(data) {
+		console.log(data);
+		addressInput.value = data.address.city + ', ' + data.address.country;
+	});
+}
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,30 @@
+
+var CACHE_STATIC_NAME = 'static-v1';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
+var STATIC_FILES = [
+	'/',
+	'/index.html',
+	'/src/js/app.js',
+	'/src/js/claim.js',
+	'/src/css/app.css',
+	'/src/css/claim.css',
+	'/src/images/students.jpg',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://code.getmdl.io/1.3.0/material.teal-red.min.css',
+	'https://fonts.googleapis.com/css?family=Open+Sans',
+	'https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js'
+];
+
+
 self.addEventListener('install', function(event) {
 	console.log('Installing SW');
+	event.waitUntil(
+		caches.open(CACHE_STATIC_NAME)
+		.then(function(cache) {
+			cache.addAll(STATIC_FILES);
+		})
+	);
 });
 
 self.addEventListener('activate', function(event) {
